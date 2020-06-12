2dcc0e4221d7a5cb020f225004d4a0cc2f70d828
Ubuntu <jaganazure@vm1.2tusw2u5mvlexc0i2kn03hj01g.bx.internal.cloudapp.net>
2020-06-12 17:01:27 +0000

    latest

---
+++ vars.tf
@@ -0,0 +1,35 @@
+variable "rgroup" {
+  type = "string"
+}
+
+variable "location" {
+  type = "string"
+}
+
+variable "prefix" {
+  type = "string"
+}
+variable "vnet_address_space" {
+  type = "string"
+}
+
+variable "subnet_address_space" {
+  type = "list"
+}
+
+variable "subnet_name" {
+  type = "list"
+}
+
+
+variable "web_network_interface" {
+  type = "string" 
+}
+
+variable "webserver_count" {
+  type = "string"
+}
+
+
+
+
